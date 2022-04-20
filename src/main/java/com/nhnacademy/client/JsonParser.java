944b4c0cdb466ad7b627cc01c68b0aef411e7f03
Hochul <hochul-y@naver.com>
2022-04-20 23:00:20 +0900

    충돌날듯?

---
+++ src/main/java/com/nhnacademy/client/JsonParser.java
@@ -0,0 +1,8 @@
+package com.nhnacademy.client;
+
+import java.util.List;
+import java.util.Map;
+
+public class JsonParser {
+    String args;
+}
--- src/main/java/com/nhnacademy/client/SimpleCurl.java
+++ src/main/java/com/nhnacademy/client/SimpleCurl.java
@@ -1,31 +1,27 @@
 package com.nhnacademy.client;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.URL;
 import java.net.URLConnection;
 import java.nio.charset.Charset;
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 
 public class SimpleCurl {
-    private BufferedReader reader;
-    private PrintWriter writer;
     private Scanner scanner = new Scanner(System.in);
-    private String inputData = "";
     private long count = 0L;
     private boolean used = false;
+
 
     public static void main(String[] argc) {
         SimpleCurl scurl = new SimpleCurl();
         scurl.compareArgv(argc);
         scurl.start(argc[1],80);
     }
-
     public void compareArgv(String[] argc){
         for (String s : argc) {
             System.out.printf(s+" ");
@@ -41,35 +37,42 @@ public class SimpleCurl {
     //정렬된 ip 가 들어가야할 것같다. port 도 default 로 지정해주고 /
     public void start(String ip, int port) {
         try {
-            String temp = "https://"+ip; // --> default 로 따로 설정해야겠다
             String line = "";
-            URL url = new URL(temp);
+            URL url = new URL(ip);
+            URL obj = new URL("https://crunchify.com");
             var inetAddress = InetAddress.getByName(url.getHost());
-            System.out.println(inetAddress.getAddress().toString());
             var clientSocket = new Socket(inetAddress, port);
             var reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            StringBuilder builder = new StringBuilder();
-            if(reader != null){
-                int count;
-                while((count = reader.read()) != - 1){
-                    builder.append((char) count);
-                }
-            }
-            System.out.println(builder.toString());
 
-            do
-            {
-                line = reader.readLine();
-                if (line == "") break;
-                System.out.println(line);
-                builder.append(line);
+            StringBuilder builder = new StringBuilder();
+            while ( (line = reader.readLine()) != null) {
+                builder.append(line).append(System.lineSeparator());
             }
-            while (true);
-            writer = new PrintWriter(clientSocket.getOutputStream());
+            var writer = new PrintWriter(clientSocket.getOutputStream());
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
+//    private void print(URL obj) throws IOException {
+//        URLConnection conn = obj.openConnection();
+//        Map<String, List<String>> map = conn.getHeaderFields();
+//
+//        System.out.println("Printing All Response Header for URL: " + obj.toString() + "\n");
+//        for (Map.Entry<String, List<String>> entry : map.entrySet()) {
+//            System.out.println(entry.getKey() + " : " + entry.getValue());
+//        }
+//
+//        System.out.println("\nGet Response Header By Key ...\n");
+//        List<String> contentLength = map.get("Content-Length");
+//        if (contentLength == null) {
+//            System.out.println("'Content-Length' doesn't present in Header!");
+//        } else {
+//            for (String header : contentLength) {
+//                System.out.println("Content-Lenght: " + header);
+//            }
+//        }
+//    }
 
     public void setUsed(boolean used) {
         this.used = used;
@@ -78,46 +81,6 @@ public class SimpleCurl {
     public boolean isUsed() {
         return used;
     }
-
 }
 
-//v    public static String crunchifyGETCallURLUtil(String crunchifyURL) {
-//        crunchifyPrint("Requested URL:" + crunchifyURL);
-//
-//        // A mutable sequence of characters. This class provides an API compatible with StringBuffer,
-//        // but with no guarantee of synchronization.
-//        StringBuilder crunchifyStringBuilder = new StringBuilder();
-//        URLConnection crunchifyURLConnection = null;
-//        InputStreamReader in = null;
-//        try {
-//        URL url = new URL(crunchifyURL);
-//        crunchifyURLConnection = url.openConnection();
-//        if (crunchifyURLConnection != null)
-//        // Set 5 second Read timeout
-//        crunchifyURLConnection.setReadTimeout(5 * 1000);
-//
-//        if (crunchifyURLConnection != null && crunchifyURLConnection.getInputStream() != null) {
-//        in = new InputStreamReader(crunchifyURLConnection.getInputStream(),
-//        Charset.defaultCharset());
-//        BufferedReader bufferedReader = new BufferedReader(in);
-//
-//        if (bufferedReader != null) {
-//        int cp;
-//        while ((cp = bufferedReader.read()) != -1) {
-//        crunchifyStringBuilder.append((char) cp);
-//        }
-//        bufferedReader.close();
-//        }
-//        }
-//        in.close();
-//        } catch (Exception e) {
-//        throw new RuntimeException("Exception while calling URL:" + crunchifyURL, e);
-//        }
-//
-//        return crunchifyStringBuilder.toString();
-//        }
-//
-//private static void crunchifyPrint(String print) {
-//        System.out.println(print);
-//        }
 
