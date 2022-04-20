9a4a76fbe90b8b2d0bfddd812aff92ba2ea01a60
유호철 <hochul-y@naver.com>
2022-04-20 22:50:56 +0900

    문제 5번까지해결

--- src/main/java/com/nhnacademy/client/SimpleCurl.java
+++ src/main/java/com/nhnacademy/client/SimpleCurl.java
@@ -1,37 +1,65 @@
 package com.nhnacademy.client;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.PrintStream;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.URL;
-import java.net.URLConnection;
-import java.nio.charset.Charset;
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class SimpleCurl {
-    private BufferedReader reader;
-    private PrintWriter writer;
-    private Scanner scanner = new Scanner(System.in);
-    private String inputData = "";
-    private long count = 0L;
-    private boolean used = false;
+    //필수 조건
+    @Parameter(required = true)
+    private String ip;
 
-    public static void main(String[] argc) {
-        SimpleCurl scurl = new SimpleCurl();
-        scurl.compareArgv(argc);
-        scurl.start(argc[1],80);
+    @Parameter(names = {"-X", "METHOD"})
+    private String method = "GET";
+
+    @Parameter(names = "-d")
+    private String data = "";
+
+    @Parameter(names = {"-v", "verbose"})
+    private boolean verbose = false;
+
+    @Parameter(names = {"-H", "Header"})
+    private List<String> customHeaders = new ArrayList<>();
+
+    public String getMethod() {
+        return method;
     }
 
-    public void compareArgv(String[] argc){
-        for (String s : argc) {
-            System.out.printf(s+" ");
-            if(s.equals("scurl")) setUsed(true);
-        }
-        if(!isUsed()) throw new IllegalArgumentException();
+    private boolean used = false;
+
+    public String getBody() {
+        return data;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public static void main(String[] argv) {
+        SimpleCurl scurl = new SimpleCurl();
+        JCommander jCommander = new JCommander(scurl);
+        jCommander.setCaseSensitiveOptions(false);
+        jCommander.parse(argv);
+
+        scurl.start(scurl.getIp(), 80, argv);
+    }
+
+    public String stringUrl(URL url, String[] argc) {
+        customHeaders.add("Content-Length: " + getBody().length());
+        return getMethod() + " /" + getMethod().toLowerCase() + " HTTP/1.1"
+            + "\n" + "Host: " + url.getHost()
+            + "\n" + String.join("\n", customHeaders)
+            + "\n"
+            + "\n" + getBody();
     }
 
     public void run(String host, int port) {
@@ -39,33 +67,33 @@ public class SimpleCurl {
     }
 
     //정렬된 ip 가 들어가야할 것같다. port 도 default 로 지정해주고 /
-    public void start(String ip, int port) {
+    public void start(String ip, int port, String[] argc) {
         try {
-            String temp = "https://"+ip; // --> default 로 따로 설정해야겠다
             String line = "";
-            URL url = new URL(temp);
+            URL url = new URL(ip);
             var inetAddress = InetAddress.getByName(url.getHost());
-            System.out.println(inetAddress.getAddress().toString());
             var clientSocket = new Socket(inetAddress, port);
             var reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            StringBuilder builder = new StringBuilder();
-            if(reader != null){
-                int count;
-                while((count = reader.read()) != - 1){
-                    builder.append((char) count);
-                }
-            }
-            System.out.println(builder.toString());
+            var writer = new PrintStream((clientSocket.getOutputStream()));
 
-            do
-            {
-                line = reader.readLine();
-                if (line == "") break;
+            //요청 라인
+            writer.print(stringUrl(url, argc));
+            //헤더 정보
+
+            //출력
+            while ((line = reader.readLine()) != null) {
+                if (!verbose) {
+                    if (!line.equals("{")) {
+                        continue;
+                    } else {
+                        verbose = true;
+                    }
+                }
                 System.out.println(line);
-                builder.append(line);
             }
-            while (true);
-            writer = new PrintWriter(clientSocket.getOutputStream());
+            reader.close();
+            writer.close();
+            clientSocket.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -78,46 +106,4 @@ public class SimpleCurl {
     public boolean isUsed() {
         return used;
     }
-
 }
-
-//v    public static String crunchifyGETCallURLUtil(String crunchifyURL) {
-//        crunchifyPrint("Requested URL:" + crunchifyURL);
-//
-//        // A mutable sequence of characters. This class provides an API compatible with StringBuffer,
-//        // but with no guarantee of synchronization.
-//        StringBuilder crunchifyStringBuilder = new StringBuilder();
-//        URLConnection crunchifyURLConnection = null;
-//        InputStreamReader in = null;
-//        try {
-//        URL url = new URL(crunchifyURL);
-//        crunchifyURLConnection = url.openConnection();
-//        if (crunchifyURLConnection != null)
-//        // Set 5 second Read timeout
-//        crunchifyURLConnection.setReadTimeout(5 * 1000);
-//
-//        if (crunchifyURLConnection != null && crunchifyURLConnection.getInputStream() != null) {
-//        in = new InputStreamReader(crunchifyURLConnection.getInputStream(),
-//        Charset.defaultCharset());
-//        BufferedReader bufferedReader = new BufferedReader(in);
-//
-//        if (bufferedReader != null) {
-//        int cp;
-//        while ((cp = bufferedReader.read()) != -1) {
-//        crunchifyStringBuilder.append((char) cp);
-//        }
-//        bufferedReader.close();
-//        }
-//        }
-//        in.close();
-//        } catch (Exception e) {
-//        throw new RuntimeException("Exception while calling URL:" + crunchifyURL, e);
-//        }
-//
-//        return crunchifyStringBuilder.toString();
-//        }
-//
-//private static void crunchifyPrint(String print) {
-//        System.out.println(print);
-//        }
-
